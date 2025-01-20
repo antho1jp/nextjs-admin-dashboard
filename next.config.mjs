fb3f0eb6a48bd98e5bb2ceb8fcee871c3f38304c
Jason Anthony <antho1jp@gmail.com>
2025-01-20 12:18:21 -0500

    fixed window build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  productionBrowserSourceMaps: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '.next/server/**/*.map': [], // Ensure `.map` files are included in tracing
+    },
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.devtool = 'source-map'; // Enable source maps
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
